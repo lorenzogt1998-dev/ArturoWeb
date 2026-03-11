888d9b71f532c4075373a4a8b7531992c8f501d7
Lorenzo Guido <lorenzogt1998@gmail.com>
2026-03-11 02:29:27 -0300

    primer commit

---
+++ src/main/java/Backend/ArturoWeb/ArturoWebApplication.java
@@ -0,0 +1,13 @@
+package Backend.ArturoWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArturoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArturoWebApplication.class, args);
+	}
+
+}
