e05092bc019babc5dae52752efe0e769c41a794f
Brisa Cordova <brisacordova2305@gmail.com>
2026-04-02 21:07:44 -0300

    Exhibition entity creada

---
+++ src/main/java/Backend/ArturoWeb/Entity/Exhibition.java
@@ -0,0 +1,94 @@
+package Backend.ArturoWeb.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "exhibitions")
+public class Exhibition {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private String location;
+    private Date startDate;
+    private Date endDate;
+    private String imageUrl;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Exhibition(Long id, String title, String description, String location, Date startDate, Date endDate, String imageUrl) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.location = location;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.imageUrl = imageUrl;
+    }
+    public Exhibition(){
+
+    }
+}
